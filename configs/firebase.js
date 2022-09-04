06bcfc1d8711e51c7cbc4799d0e8ab0ae129ca07
Dimitar <wopz1337@gmail.com>
2022-09-04 21:11:42 +0300

    add firebase to project

---
+++ configs/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+//Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAfXS_gZr77vCp1BKqSZjQ33D3e5PFwE",
+  authDomain: "blog-post-app-17eeb.firebaseapp.com",
+  projectId: "blog-post-app-17eeb",
+  storageBucket: "blog-post-app-17eeb.appspot.com",
+  messagingSenderId: "267269319948",
+  appId: "1:267269319948:web:df2df0412950872aa4ed29",
+  measurementId: "G-53KGHQQ7PM",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
