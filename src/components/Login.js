48e87b959d0a9a73f732a2a20320cab6f984b83a
Dimitar <wopz1337@gmail.com>
2022-09-05 10:33:12 +0300

    Add Button For Sign In With Google;

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,19 @@
+import { Box, Container } from "@mui/material";
 import React from "react";
-
+import GoogleButton from "react-google-button";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box sx={{ paddingTop: "2rem" }}>
+        <GoogleButton sx={{ backgroundColor: "red" }} />
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
