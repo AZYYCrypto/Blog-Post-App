971ae1028b566e424205f2f7611596d30c234487
Dimitar <wopz1337@gmail.com>
2022-09-05 10:18:46 +0300

    Add Title  for About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,14 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container>
+      <Typography variant="h5" sx={{ textAlign: "center", marginTop: "1rem" }}>
+        About Me
+      </Typography>
+    </Container>
+  );
 };
 
 export default About;
