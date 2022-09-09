6080cfb0e4eb38e4d79f804ac11f7423983e4765
Dimitar <wopz1337@gmail.com>
2022-09-10 01:31:23 +0300

    Add Button For Create Post

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,14 +1,17 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
-
+import CreatePost from "./CreatePost";
+import { Link } from "react-router-dom";
 const MyAccount = () => {
   return (
     <Container>
       <Typography variant="h5" sx={{ textAlign: "center", marginTop: "1rem" }}>
-        My Account:
+        My Account
       </Typography>
+      <Button variant="contained" component={Link} to="/create-post">
+        Create Post
+      </Button>
     </Container>
   );
 };
-
 export default MyAccount;
