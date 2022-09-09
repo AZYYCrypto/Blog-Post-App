0c46e94a0c049b0a7a281805c2b9390ed3825ea6
Dimitar <wopz1337@gmail.com>
2022-09-10 00:51:02 +0300

    Add MUI Components For Contact and MyAccount Components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Container>
+      <Typography variant="h5" sx={{ textAlign: "center", marginTop: "1rem" }}>
+        Contact
+      </Typography>
+    </Container>
+  );
 };
 
 export default Contact;
--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,7 +1,14 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 const MyAccount = () => {
-  return <div>MyAccount</div>;
+  return (
+    <Container>
+      <Typography variant="h5" sx={{ textAlign: "center", marginTop: "1rem" }}>
+        My Account:
+      </Typography>
+    </Container>
+  );
 };
 
 export default MyAccount;
