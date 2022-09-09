12067b831969e875cfc3a8f9d3829922c48f023e
Dimitar <wopz1337@gmail.com>
2022-09-10 00:49:36 +0300

    Link Pages In Footer & Add Copyright Box Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,58 @@
-import { Container } from "@mui/material";
-import React from "react";
-
-const Footer = () => {
-  return <Container>Footer</Container>;
-};
-
-export default Footer;
+import { Container, Grid, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <Box
+      px={{ xs: 3, sm: 10 }}
+      py={{ xs: 5, sm: 5 }}
+      marginTop={5}
+      bgcolor="#1976D2"
+      color="white"
+      component="footer"
+    >
+      <Container>
+        <Grid container spacing={0} justifyContent="center">
+          <Grid item xs={12} sm={1.3}>
+            <Box
+              to="/about"
+              component={Link}
+              sx={{ color: "white", textDecoration: "none" }}
+            >
+              About
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={1.3}>
+            <Box
+              to="/contact"
+              component={Link}
+              sx={{ color: "white", textDecoration: "none" }}
+            >
+              Contact
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={1.3}>
+            <Box
+              to="/"
+              component={Link}
+              sx={{ color: "white", textDecoration: "none" }}
+            >
+              Policy
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={1.3}>
+            <Box
+              to="/"
+              component={Link}
+              sx={{ color: "white", textDecoration: "none" }}
+            >
+              Term & Conditions
+            </Box>
+          </Grid>
+        </Grid>
+        <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
+          Copyright © {new Date().getFullYear()} Dimitar Sabev
+        </Box>
+      </Container>
+    </Box>
+  );
+}
