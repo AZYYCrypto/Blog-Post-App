4eb6630f20b646d2f819864c156cd685e2ce1211
Dimitar <wopz1337@gmail.com>
2022-09-10 01:32:07 +0300

    Add 1 Heading For Create Post Component

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,14 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <Container>
+      <Typography variant="h4" sx={{ textAlign: "center", marginTop: "1rem" }}>
+        Create Post
+      </Typography>
+    </Container>
+  );
 };
 
 export default CreatePost;
