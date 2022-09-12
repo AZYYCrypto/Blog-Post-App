490fa067c6e3ccc4f8645ad3f62e06f24fd4eae0
Dimitar <wopz1337@gmail.com>
2022-09-12 17:47:31 +0300

    Add Create Post Form

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,12 +1,50 @@
-import { Container, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Paper,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const CreatePost = () => {
   return (
-    <Container>
-      <Typography variant="h4" sx={{ textAlign: "center", marginTop: "1rem" }}>
-        Create Post
-      </Typography>
+    <Container maxWidth="sm">
+      <Paper
+        elevation={24}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "2.5rem",
+          gap: "0.5rem",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{ textAlign: "center", marginTop: "1rem" }}
+        >
+          Create a Post
+        </Typography>
+
+        <TextField
+          id="outlined-title-input"
+          label="Title"
+          type="text"
+          sx={{ margin: "1rem" }}
+        />
+        <TextField
+          variant="outlined"
+          label="Description"
+          multiline
+          rows={5}
+          rowsMax={10}
+          sx={{ margin: "1rem" }}
+        />
+        <Button variant="contained" sx={{ margin: "1rem" }}>
+          Submit Post
+        </Button>
+      </Paper>
     </Container>
   );
 };
