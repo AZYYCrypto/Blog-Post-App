23cb37931d3e8a02581e424a8e805ccdfe8277db
Dimitar <wopz1337@gmail.com>
2022-09-12 12:30:08 +0300

    Add Auth Function From FireBase

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 //Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyCqAfXS_gZr77vCp1BKqSZjQ33D3e5PFwE",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
