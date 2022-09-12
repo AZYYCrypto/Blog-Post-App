d2b7bb8c4250542a0b23a8a482abfcb712afc79b
Dimitar <wopz1337@gmail.com>
2022-09-12 21:17:39 +0300

    Add Firebase/Storage In Firebase File

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 //Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyCqAfXS_gZr77vCp1BKqSZjQ33D3e5PFwE",
@@ -17,3 +18,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
