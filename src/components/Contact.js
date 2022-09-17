67da9cb1a72b193c2e927d5fa88e58ad7a0bef2a
Dimitar <wopz1337@gmail.com>
2022-09-17 14:18:54 +0300

    Added Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,96 @@
-import { Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Contact = () => {
   return (
-    <Container>
-      <Typography variant="h5" sx={{ textAlign: "center", marginTop: "1rem" }}>
-        Contact
-      </Typography>
-    </Container>
+    <Grid sx={{ marginTop: "2rem" }}>
+      <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Contact
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            gutterBottom
+          >
+            Fill up the form and i will get back to you within 24 hours.
+          </Typography>
+          <Box component="form">
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  placeholder="Enter first name"
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  placeholder="Enter last name"
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="email"
+                  placeholder="Enter email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="number"
+                  placeholder="Enter phone number"
+                  label="Phone"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Message"
+                  multiline
+                  rows={4}
+                  placeholder="Type your message here"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
