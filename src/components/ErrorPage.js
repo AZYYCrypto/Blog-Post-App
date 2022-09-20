70add39ca1f721a3b02d47bef8da5b2b27827b3d
Dimitar <wopz1337@gmail.com>
2022-09-20 21:38:31 +0300

    Added Error Page Content With MUI

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,7 +1,19 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "1rem",
+      }}
+    >
+      <Typography variant="h4">Page Not Found</Typography>
+    </Container>
+  );
 };
 
 export default ErrorPage;
