eca105e28211f71800a4987cd945a3a4b679447c
Dimitar <wopz1337@gmail.com>
2022-10-06 00:09:15 +0300

    Remove Box , Link MUI Component Import Because I Dont Use It At The Moment

--- src/components/TermsAndConditions.js
+++ src/components/TermsAndConditions.js
@@ -1,6 +1,5 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const TermsAndConditions = () => {
   return (
