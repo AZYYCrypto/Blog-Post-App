7158ad51be7c652f9e72d5831f5a061d96675edc
Dimitar <wopz1337@gmail.com>
2022-10-07 19:03:49 +0300

    Fix Bugs In Post Template

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,18 +5,12 @@ import {
   CardContent,
   CardMedia,
   Container,
-  Box,
   Typography,
   Avatar,
   CardHeader,
 } from "@mui/material";
-import ShareIcon from "@mui/icons-material/Share";
-import EditIcon from "@mui/icons-material/Edit";
-import { FavoriteBorder, Favorite } from "@mui/icons-material/";
-import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../configs/firebase";
-import DeleteDocIcon from "./DeleteDocIcon.js";
 import Loading from "./Loading";
 import { Link } from "react-router-dom";
 const Post = () => {
@@ -64,69 +58,12 @@ const Post = () => {
         postList.map(
           ({ id, title, author, description, createdAt, imageUrl }) => {
             return (
-              // <Card sx={{ borderRadius: "20px" }} key={id}>
-              //   <CardHeader
-              //     avatar={<Avatar aria-label="recipe"></Avatar>}
-              //     action={
-              //       <>
-              //         {/* <IconButton aria-label="settings">
-              //         <EditIcon />
-              //       </IconButton> */}
-
-              //         <DeleteDocIcon
-              //           setPostList={setPostList}
-              //           id={id}
-              //           imageUrl={imageUrl}
-              //         />
-              //       </>
-              //     }
-              //     title={title}
-              //     subheader={`${author.name} · ${createdAt}`}
-              //   />
-              //   <CardMedia
-              //     component="img"
-              //     height="530"
-              //     image={imageUrl}
-              //     alt="Paella dish"
-              //   />
-              //   <CardContent>
-              //     <Typography variant="body2" color="text.secondary">
-              //       {description}
-              //     </Typography>
-              //   </CardContent>
-              //   <CardActions disableSpacing>
-              //     <IconButton aria-label="add to favorites">
-              //       <Checkbox
-              //         icon={<FavoriteBorder />}
-              //         checkedIcon={<Favorite sx={{ color: "red" }} />}
-              //       />
-              //     </IconButton>
-              //     <IconButton aria-label="share">
-              //       <ShareIcon />
-              //     </IconButton>
-              //   </CardActions>
-              // </Card>
               <Card
                 sx={{ borderRadius: "20px", textDecoration: "none" }}
                 component={Link}
                 to={`/post/${id}`}
               >
-                <CardHeader
-                  avatar={<Avatar aria-label="recipe"></Avatar>}
-                  action={
-                    <>
-                      {/* <IconButton aria-label="settings">
-                      <EditIcon />
-                    </IconButton> */}
-
-                      <DeleteDocIcon
-                        setPostList={setPostList}
-                        id={id}
-                        imageUrl={imageUrl}
-                      />
-                    </>
-                  }
-                />
+                <CardHeader avatar={<Avatar aria-label="recipe"></Avatar>} />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
                     {title}
