70da83693281dd53ae8d0e6eb7d2fa7dad23eb6d
Dimitar <wopz1337@gmail.com>
2022-10-13 18:32:18 +0300

    Use User Photo

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -144,7 +144,11 @@ const AppMenu = () => {
             <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Account">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar src="https://secure.gravatar.com/avatar/136cb45477fdba38eeafeb0f222414a2?s=500&d=mm&r=g" />
+                  {user ? (
+                    <Avatar src={user?.photoURL} />
+                  ) : (
+                    <Avatar src="https://secure.gravatar.com/avatar/136cb45477fdba38eeafeb0f222414a2?s=500&d=mm&r=g" />
+                  )}
                 </IconButton>
               </Tooltip>
 
